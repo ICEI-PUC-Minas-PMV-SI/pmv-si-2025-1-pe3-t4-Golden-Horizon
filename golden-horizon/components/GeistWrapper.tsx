b51cdbd5526b8e848fa2033fae362dc0ca586d84
Markus Machel <machelmarkus@gmail.com>
2025-05-22 01:58:57 -0300

    regras do linter, footer, prettier

--- golden-horizon/components/GeistWrapper.tsx
+++ golden-horizon/components/GeistWrapper.tsx
@@ -1,12 +1,16 @@
-'use client'
+"use client";
 
-import { GeistProvider, CssBaseline } from '@geist-ui/react'
+import { GeistProvider, CssBaseline } from "@geist-ui/react";
 
-export default function GeistWrapper({ children }: { children: React.ReactNode }) {
+export default function GeistWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <GeistProvider>
       <CssBaseline />
       {children}
     </GeistProvider>
-  )
+  );
 }
--- golden-horizon/eslint.config.mjs
+++ golden-horizon/eslint.config.mjs
@@ -1,16 +1,39 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettierConfig from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const config = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+  ),
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  prettierConfig,
 ];
 
-export default eslintConfig;
+export default config;
