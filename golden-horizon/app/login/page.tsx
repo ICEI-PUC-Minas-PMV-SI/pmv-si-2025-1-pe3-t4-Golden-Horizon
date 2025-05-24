6d7e48c52b3c99d33bb91e8b55333ebfe0a33b9a
Btriz <44041066+Btriz@users.noreply.github.com>
2025-05-24 19:24:19 -0300

    adiciona nova página de login

---
+++ golden-horizon/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Login</h1>
+      <p className="text-lg">This is the login page.</p>
+    </main>
+  );
+}
--- golden-horizon/components/login/LoginBtn.tsx
+++ golden-horizon/components/login/LoginBtn.tsx
@@ -4,13 +4,15 @@ import { Button } from "@geist-ui/react";
 export default function LoginBtn() {
   return (
     <Button
-      type="secondary"
       auto
       scale={1.2}
       className="!bg-[#7C6A46] !text-white !font-bold hover:!bg-amber-700 !border-none"
       placeholder={undefined}
       onPointerEnterCapture={undefined}
       onPointerLeaveCapture={undefined}
+      onClick={() => {
+        window.location.href = "/login";
+      }}
     >
       Entrar / Registrar
     </Button>
